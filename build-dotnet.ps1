a230fb92c2fde4d2805ebe887a33154c7bb83244
Charles L Flatt <charles@tfhome.us>
2022-06-15 12:41:26 -0600

    Complete working build script

--- build-dotnet.ps1
+++ build-dotnet.ps1
@@ -26,6 +26,10 @@ Write-Message "## SETTINGS"
 $WorkingDirectory = Get-Location
 $ProjectDirectory = Join-Path $WorkingDirectory $SourceDirectory $ProjectName
 $PackageDirectory = Join-Path $WorkingDirectory "package" $ProjectName
+$PackageDirectoryWindows = Join-Path $PackageDirectory "windows"
+$PackageDirectoryAndroid = Join-Path $PackageDirectory "android"
+$PackageDirectoryIos = Join-Path $PackageDirectory "ios"
+$PackageDirectoryMac = Join-Path $PackageDirectory "mac"
 $TestProjectDirectory = Join-Path $WorkingDirectory $SourceDirectory $TestProjectName
 $TestResultsDirectory = Join-Path $WorkingDirectory "package" "testresults"
 $CoverageResultsDirectory = Join-Path $WorkingDirectory "package" "coverageresults"
@@ -38,6 +42,10 @@ Write-Message "ProjectName              $ProjectName"
 Write-Message "WorkingDirectory         $WorkingDirectory"
 Write-Message "ProjectDirectory         $ProjectDirectory"
 Write-Message "PackageDirectory         $PackageDirectory"
+Write-Message "PackageDirectoryWindows  $PackageDirectoryWindows"
+Write-Message "PackageDirectoryAndroid  $PackageDirectoryAndroid"
+Write-Message "PackageDirectoryIos      $PackageDirectoryIos"
+Write-Message "PackageDirectoryMac      $PackageDirectoryMac"
 Write-Message "TestProjectName          $TestProjectName"
 Write-Message "TestProjectDirectory     $TestProjectDirectory"
 Write-Message "TestResultsDirectory     $TestResultsDirectory"
@@ -56,14 +64,17 @@ try {
     Write-Message "## BUILD"
     Run { dotnet build $ProjectDirectory -c Release -t:Rebuild -p:WarningLevel=1 -warnAsMessage:"CS1591" }
 
-    if (-Not($NoTest)) {
+    if (-Not($NoTest) -and $TestProjectName -ne '') {
         Write-Message "## TEST"
         $LogFileName = "$TestProjectName" + ".trx"
         Run { dotnet test $TestProjectDirectory --results-directory $TestResultsDirectory --logger "trx;LogFileName=$LogFileName" }
     }
 
     Write-Message "# PUBLISH"
-    Run { dotnet publish $ProjectDirectory  -c Release --output $PackageDirectory }
+    Run { dotnet publish $ProjectDirectory -c Release --output $PackageDirectoryWindows --framework net6.0-windows10.0.19041.0}
+    Run { dotnet publish $ProjectDirectory -c Release --output $PackageDirectoryAndroid --framework net6.0-android}
+    # Run { dotnet publish $ProjectDirectory -c Release --output $PackageDirectoryIos     --framework net6.0-ios}
+    # Run { dotnet publish $ProjectDirectory -c Release --output $PackageDirectoryMac     --framework net6.0-maccatalyst}
 }
 catch {}
 finally {
